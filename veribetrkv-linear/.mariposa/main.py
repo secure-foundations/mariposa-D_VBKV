6d347c6eff5d54d7d8df45cb08330a6c641f5487
yizhou7 <yizhou7@outlook.com>
2023-07-25 18:50:26 -0400

    update main.py

--- veribetrkv-linear/.mariposa/main.py
+++ veribetrkv-linear/.mariposa/main.py
@@ -30,15 +30,34 @@ nl_enable_files = ["lib/Math/Nonlinear.i.dfy",
 
 nl_enable_files = set([os.path.realpath(f) for f in nl_enable_files])
 
-# cur_path = os.path.realpath(".")
+def emit_export_cmds():
+    cur_path = os.path.realpath(".")
+    def print_export_commands():
+        for file_path in open(".mariposa/unique_imports.txt").readlines():
+            file_path = file_path.strip() 
+            command = ".dafny/dafny/Scripts/dafny /timeLimit:1 /compile:0 /proverLog:.mariposa/queries/@FILE@xxx@PROC@ "
+            if file_path not in nl_enable_files:
+                command += " /noNLarith "
+            # file_path = file_path.replace(cur_path, ".")
+            # do not give a long time for verification, we only need the query
+            command += file_path
+            print(command) 
 
-for file_path in open(".mariposa/unique_imports.txt").readlines():
-    file_path = file_path.strip() 
-    command = ".dafny/dafny/Scripts/dafny /timeLimit:1 /compile:0 /proverLog:.mariposa/queries/@FILE@xxx@PROC@ "
-    if file_path not in nl_enable_files:
-        command += " /noNLarith "
-    # file_path = file_path.replace(cur_path, ".")
-    # do not give a long time for verification, we only need the query
-    command += file_path
-    print(command) 
-    
+import re, subprocess 
+subroot = ".mariposa/queries/"
+files = os.listdir(subroot)
+pattern = re.compile("\-n[0-9]+")
+
+for file in files:
+    match = re.search(pattern, file)
+    path = subroot + "\\" + file
+    new_file = file
+    if match:
+        res = subprocess.run(f"rg -e 'set-info :boogie-vc-id' {path}", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout = res.stdout.decode("utf-8").strip()
+        full = stdout[len("(set-info :boogie-vc-id "):-1].replace("$", "_")
+        new_file = file.split("xxx")[0] + "xxx" + full
+    new_file = new_file.replace("-home-yizhou7-linear-veribetrkv-artifact-veribetrkv-linear-", "")
+    cmd = f"mv {path} {subroot + new_file}.smt2"
+    # print(cmd)
+    os.system(cmd)
